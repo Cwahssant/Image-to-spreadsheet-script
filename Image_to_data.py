2c7fb23c6291c632e4a6a726ae0c851f2a94a4e1
Cwahssant <lohytiger@gmail.com>
2026-06-02 11:11:47 -0700

    started checking for missing libraries function

--- Image_to_data.py
+++ Image_to_data.py
@@ -1,4 +1,12 @@
 
 
-def __main__():
-    return
+def main():
+    print("I'm in Image_to_data.py")
+
+#Checks whether the user has the required dependencies installed. If not then it'll download them.
+def check_dependencies():
+    required = ['easyocr', 'csv']
+    missing = []
+
+    for library in required:
+        print(library)
