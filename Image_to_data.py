d1496aaec8fe023ccc21d0f192b7b2d9f8aba291
Cwahssant <lohytiger@gmail.com>
2026-06-10 21:42:51 -0700

    moved check_dependencies function to install_missing_libraries.py

--- Image_to_data.py
+++ Image_to_data.py
@@ -1,12 +1,5 @@
-
+import install_missing_libraries
+import csv
 
 def main():
-    print("I'm in Image_to_data.py")
-
-#Checks whether the user has the required dependencies installed. If not then it'll download them.
-def check_dependencies():
-    required = ['easyocr', 'csv']
-    missing = []
-
-    for library in required:
-        print(library)
+    install_missing_libraries.check_dependencies()
