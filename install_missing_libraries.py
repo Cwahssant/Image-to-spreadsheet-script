0205f7737059d3b2257e03a2a54822abf30cc7a4
Cwahssant <lohytiger@gmail.com>
2026-06-14 22:17:03 -0700

    script is able to install missing libraries

--- install_missing_libraries.py
+++ install_missing_libraries.py
@@ -1,11 +1,22 @@
-from pkgutil import iter_importers
+from sys import executable
+from pkgutil import iter_modules
+from subprocess import run
 
 #Checks whether the user has the required dependencies installed. If not then it'll download them.
 def check_dependencies():
+    installed_modules = list(iter_modules())
     required = ['easyocr']
     missing = []
 
     for library in required:
-        print(iter_importers(library))
+        if library not in installed_modules:
+            missing.append(library)
 
-    
+    if len(missing) > 0:
+        install_libraries(missing)
+
+
+#Installs the missing libraries onto the user's device        
+def install_libraries(libraries_list):
+    for library in libraries_list:
+        run([executable, '-m', 'pip', 'install', library])
