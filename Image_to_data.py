1a90e21308bee5fd119698845504f08ad5195c55
Cwahssant <lohytiger@gmail.com>
2026-06-16 09:16:00 -0700

    added initial functions for reading and writing information to a file

--- Image_to_data.py
+++ Image_to_data.py
@@ -1,5 +1,25 @@
 import install_missing_libraries
 import csv
+import sys
 
-def main():
+def main(file_name):
+    output_file_name = file_name.split('.')[0]
+
     install_missing_libraries.check_dependencies()
+
+    read_file(file_name)
+
+
+#Returns the information from the image file 
+def read_file(file):
+    try:
+        ptr = open(file, "r")
+
+        ptr.close()
+    except:
+        print(file, "does not exist")
+
+
+# Writes information to a csv file
+def write_info(file_ptr):
+    return
