1fdc754b5909626a3f0e4da79ce50888da107935
Cwahssant <lohytiger@gmail.com>
2026-06-17 08:26:29 -0700

    this file checks for missing dependencies now

--- get_input.py
+++ get_input.py
@@ -1,8 +1,10 @@
 import sys
 import Image_to_data
+import install_missing_libraries
 
 if len(sys.argv) < 2:
     print("Please provide image file")
     sys.exit(1)
 
+install_missing_libraries.check_dependencies()
 Image_to_data.main(sys.argv[1])
