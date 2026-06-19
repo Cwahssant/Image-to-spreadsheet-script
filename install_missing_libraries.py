d5ffebfbe5ef6b0ba9043bc034d6c5a50bf2b9c5
Cwahssant <lohytiger@gmail.com>
2026-06-19 11:34:31 -0700

    install_missing_libraries.py doesn't install missing dependencies for users anymore

--- install_missing_libraries.py
+++ install_missing_libraries.py
@@ -1,25 +1,21 @@
-from sys import executable
-from pkgutil import iter_modules
-from subprocess import run
+import sys
 from importlib.util import find_spec
+from shutil import which
 
 
 #Checks whether the user has the required dependencies installed. If not then it'll download them.
 def check_dependencies():
-    installed_modules = list(iter_modules())
     required = ['pytesseract']
-    missing = []
+    missing = False
     
     for library in required:
-        if library not in installed_modules:
-            missing.append(library)
-
-    if len(missing) > 0:
-        install_libraries(missing)
-
-
-#Installs the missing libraries onto the user's device        
-def install_libraries(libraries_list):
-    for library in libraries_list:
         if find_spec(library) is None:
-            run([executable, '-m', 'pip', 'install', library], check = True)
+            print("missing: ", library + "\nType \"pip install pytesseract\" in the command line to install.")
+            missing = True
+
+    if which("tesseract") is None:
+        print("Please install Tesseract at https://tesseract-ocr.github.io/tessdoc/Installation.html")
+        missing = True
+
+    if missing:
+        sys.exit(1)
