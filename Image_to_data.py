6b78d9f8a01c870deba74a9920ebe9a2fe38ca39
Cwahssant <lohytiger@gmail.com>
2026-06-23 10:46:04 -0700

    Script is able to write data to csv file

--- Image_to_data.py
+++ Image_to_data.py
@@ -7,7 +7,17 @@ def main(file_name):
     output_file_name = file_name.split('.')[0] + '.csv'
     
     pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\tiger\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe' #Delete later.
-    read_file(file_name)
+    row_data = read_file(file_name)
+
+    with open(output_file_name, "w", newline = '') as ptr:
+
+        if ptr == None:
+            print("Error opening", output_file_name)
+            sys.exit(1)
+
+        write_info(ptr, row_data)
+
+    ptr.close()
 
 
 #Returns the information from the image file 
@@ -18,15 +28,18 @@ def read_file(file):
         print("Unable to open", file)
         sys.exit(1)
 
-    prepped_data = []
+    row_data = []
 
     dat_list = dat.splitlines(keepends = True)
     for line in dat_list:
-        prepped_data.append(line.split())
+        row_data.append(line.split())
 
-    return prepped_data
+    return row_data
 
 
 # Writes information to a csv file
 def write_info(file_ptr, info):
-    return
+    data_writer = csv.writer(file_ptr)
+
+    for row in info:
+        data_writer.writerow(row)
