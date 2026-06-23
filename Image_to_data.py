278d9e008dea69f9956aaa4fe581b4bc14b60a72
Cwahssant <lohytiger@gmail.com>
2026-06-23 08:58:51 -0700

    Script is able to read data from an image file

--- Image_to_data.py
+++ Image_to_data.py
@@ -1,25 +1,32 @@
-import install_missing_libraries
 import csv
 import sys
+from PIL import Image
+import pytesseract
 
 def main(file_name):
-    output_file_name = file_name.split('.')[0]
-
-    install_missing_libraries.check_dependencies()
-
+    output_file_name = file_name.split('.')[0] + '.csv'
+    
+    pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\tiger\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe' #Delete later.
     read_file(file_name)
 
 
 #Returns the information from the image file 
 def read_file(file):
     try:
-        ptr = open(file, "r")
-
-        ptr.close()
+        dat = pytesseract.image_to_string(Image.open(file))    
     except:
-        print(file, "does not exist")
+        print("Unable to open", file)
+        sys.exit(1)
+
+    prepped_data = []
+
+    dat_list = dat.splitlines(keepends = True)
+    for line in dat_list:
+        prepped_data.append(line.split())
+
+    return prepped_data
 
 
 # Writes information to a csv file
-def write_info(file_ptr):
+def write_info(file_ptr, info):
     return
