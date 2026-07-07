83c6fc9e7f5a047f45fd36cd112115dd8e2d6a39
Cwahssant <lohytiger@gmail.com>
2026-07-07 09:00:33 -0700

    improved accuracy of text recognition

--- Image_to_data.py
+++ Image_to_data.py
@@ -4,11 +4,14 @@ from PIL import Image
 import pytesseract
 
 def main(file_name):
-    output_file_name = file_name.split('.')[0] + '.csv'
-    
+    file_extension_location = file_name.rfind('.')
+
+    output_file_name = file_name[0:file_extension_location] + '.csv'
+
     pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\tiger\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe' #Delete later.
     row_data = read_file(file_name)
-
+    row_data = remove_empty_lists(row_data)
+    print(row_data)
     with open(output_file_name, "w", newline = '') as ptr:
 
         if ptr == None:
@@ -18,19 +21,23 @@ def main(file_name):
         write_info(ptr, row_data)
 
     ptr.close()
-
+    
 
 #Returns the information from the image file 
 def read_file(file):
+    config = r'--oem 3 --psm %d' % 6
+
     try:
-        dat = pytesseract.image_to_string(Image.open(file))    
+        dat = pytesseract.image_to_string(Image.open(file), config = config)
     except:
         print("Unable to open", file)
         sys.exit(1)
 
+    
     row_data = []
 
     dat_list = dat.splitlines(keepends = True)
+    
     for line in dat_list:
         row_data.append(line.split())
 
@@ -40,6 +47,31 @@ def read_file(file):
 # Writes information to a csv file
 def write_info(file_ptr, info):
     data_writer = csv.writer(file_ptr)
-
-    for row in info:
+    
+    for row in info:   
         data_writer.writerow(row)
+
+
+'''
+Enhances the provided image file to improve accuracy of text recognition
+
+This method will return the name of the enhanced image file
+'''
+def enhance_image(file):
+    #Enhancing image work
+
+    file_components = file.split(".")
+    return file_components[0] + "_ENHANCED." + file_components[1]
+
+
+#Takes a list of lists (the data) and removes all empty lists
+def remove_empty_lists(data):
+    result = []
+
+    for row in data:
+        if len(row) == 0:
+            continue
+
+        result.append(row)
+
+    return result 
