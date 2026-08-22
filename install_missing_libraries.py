2574aee43c8ce7d7cb3bf8ef47e7eff7e3289f42
Cwahssant <lohytiger@gmail.com>
2026-08-21 17:49:14 -0700

    added open-cv as a dependency

--- install_missing_libraries.py
+++ install_missing_libraries.py
@@ -3,19 +3,19 @@ from importlib.util import find_spec
 from shutil import which
 
 
-#Checks whether the user has the required dependencies installed. If not then it'll download them.
+#Checks whether the user has the required dependencies installed.
 def check_dependencies():
-    required = ['pytesseract']
+    required = ['pytesseract', 'cv2']
     missing = False
     
     for library in required:
         if find_spec(library) is None:
-            print("missing: ", library + "\nType \"pip install pytesseract\" in the command line to install.")
+            print(f"missing: {library} \nType \"pip install {library}\" in the command line to install.")
             missing = True
 
-    if which("tesseract") is None:
-        print("Please install Tesseract at https://tesseract-ocr.github.io/tessdoc/Installation.html\n or Tesseract is not in your PATH")
-        missing = True
+    #if which("tesseract") is None:
+        #print("Please install Tesseract at https://tesseract-ocr.github.io/tessdoc/Installation.html\n or Tesseract is not in your PATH")
+        #missing = True
 
     if missing:
         sys.exit(1)
