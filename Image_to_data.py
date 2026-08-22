a458417606b336ad48d4320073f47319a8837e56
Cwahssant <lohytiger@gmail.com>
2026-08-21 17:47:31 -0700

    added a condition to determine how much image preprocessing is done

--- Image_to_data.py
+++ Image_to_data.py
@@ -9,8 +9,8 @@ def main(file_name):
     output_file_name = file_name[0:file_extension_location] + '.csv'
 
     pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\tiger\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe' #Delete later.
-    #enhance_image(file_name)
-    row_data = read_file(file_name)
+    img_file = enhance_image(file_name)
+    row_data = read_file(img_file)
     row_data = remove_empty_lists(row_data)
     
     with open(output_file_name, "w", newline = '') as ptr:
@@ -19,22 +19,19 @@ def main(file_name):
             print("Error opening", output_file_name)
             sys.exit(1)
 
-        #write_info(ptr, row_data)
+        write_info(ptr, row_data)
 
     ptr.close()
     
 
 #Returns the information from the image file 
 def read_file(file):
-    config = r'--oem 3 --psm %d' % 6
+    config = r'--oem 3 --psm %d' %6
 
     try:
         img = Image.open(file)
-        dat = pytesseract.image_to_data(img, config = config, output_type='data.frame')
+        dat = pytesseract.image_to_string(img, config = config)
         img.close()
-
-        dat = dat[dat.text.notnull()]
-        print(dat.head())
     except:
         print("Unable to open", file)
         sys.exit(1)
@@ -42,10 +39,10 @@ def read_file(file):
     
     row_data = []
 
-    #dat_list = dat.splitlines(keepends = True)
+    dat_list = dat.splitlines(keepends = True)
     
-    #for line in dat_list:
-        #row_data.append(line.split())
+    for line in dat_list:
+        row_data.append(line.split())
 
     return row_data
 
@@ -84,12 +81,16 @@ def enhance_image(file):
         print(f"unable to read {file}")
         sys.exit(1)
 
-    threshold_img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-    cv2.resize(threshold_img, None, fx = 2, fy = 2, interpolation= cv2.INTER_CUBIC)
-    blurred_img = cv2.GaussianBlur(threshold_img, (5, 5), 0)
+    img = cv2.resize(img, None, fx = 2, fy = 2, interpolation = cv2.INTER_CUBIC)
+    
+    sharpness = cv2.Laplacian(img, cv2.CV_64F).var()
 
-    currDPI = blurred_img.info.get('dpi') #change img name
-    if currDPI[0] < 300 or currDPI [1] < 300:
-        blurred_img.save(result, dpi=(300,300)) #change img name
+    if sharpness > 150:
+        img = cv2.GaussianBlur(img, (3, 3), 0)
+    else:
+        img = cv2.GaussianBlur(img, (5, 5), 0)
+        _, img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    
+    cv2.imwrite(result, img)
 
     return result
